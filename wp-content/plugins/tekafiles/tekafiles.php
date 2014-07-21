3be2db36baba4742b73d215faa5966ea9ffed701
Andres Londono <andres@foxrock.co>
2014-07-21 16:03:04 -0500

    Teka files plugin startup

---
+++ wp-content/plugins/tekafiles/tekafiles.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Teka Files
+ * Plugin URI: http://foxrock.co
+ * Description: Teka file administrator.
+ * Version: 0.1
+ * Author: Andres Londono
+ * Author URI: http://www.foxrock.co
+ * License: GPL2
+ */
